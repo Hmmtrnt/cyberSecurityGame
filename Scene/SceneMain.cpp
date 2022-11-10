8312c78c33f6818c43ad1747e03e4b97684a976b
Hmmtrnt <rento5310@gmail.com>
2022-11-10 18:33:33 +0900

    main画面レイアウト完成

--- Scene/SceneMain.cpp
+++ Scene/SceneMain.cpp
@@ -6,14 +6,32 @@
 
 namespace
 {
-	// 背景色
-	const int kBackgroundColor = GetColor(200, 200, 200);	// 灰色
+	// 文字列
+	const char* const kURL = "URL →";
+	const char* const kText = "本文→";
+	// 色
+	const int kColorG = GetColor(200, 200, 200);	// 灰色
+	const int kColorB = GetColor(0, 0, 0);			// 黒
+	// URLの枠の座標
+	constexpr int kURLFrameX = 350;
+	constexpr int kURLFrameY = 50;
+	// URLの枠のサイズ
+	constexpr int kSizeURLFrameX = 400;
+	constexpr int kSizeURLFrameY = 130;
+	// 本文の枠の座標
+	constexpr int kTextFrameX = 350;
+	constexpr int kTextFrameY = 230;
+	// 本文の枠のサイズ
+	constexpr int kSizeTextFrameX = 400;
+	constexpr int kSizeTextFrameY = 300;
 	// フェード関連
 	constexpr int kFadeBright = 0;	// 処理
 	constexpr int kFadeSpeed = 7;	// 速度
 }
 
 SceneMain::SceneMain() :
+	m_hBackGround(-1),
+	m_fontHandle(-1),
 	m_fadeBright(0),
 	m_fadeSpeed(0)
 {
@@ -21,8 +39,15 @@ SceneMain::SceneMain() :
 
 void SceneMain::init()
 {
+	m_hBackGround = LoadGraph("data/SceneMain.png");
+	m_fontHandle = CreateFontToHandle(NULL, 30, 4);
 	m_fadeBright = kFadeBright;	// フェード処理
 	m_fadeSpeed = kFadeSpeed;	// フェード速度
+}
+
+void SceneMain::end()
+{
+	DeleteFontToHandle(m_fontHandle);
 }
 
 SceneBase* SceneMain::update()
@@ -68,8 +93,6 @@ void SceneMain::draw()
 {
 	// 描画の輝度
 	SetDrawBright(m_fadeBright, m_fadeBright, m_fadeBright);
-
-
 	// 背景
-	DrawBox(0, 0, Game::kScreenWidth, Game::kScreenHeight, kBackgroundColor, true);
+	DrawGraph(0, 0, m_hBackGround, true);
 }
--- Scene/SceneMain.h
+++ Scene/SceneMain.h
@@ -9,13 +9,17 @@ public:
 
 
 	virtual void init() override;
-	virtual void end() override {}
+	virtual void end() override;
 
 	virtual SceneBase* update() override;
 	virtual void draw() override;
 private:
 	// キーボードの入力状態
 	int m_key[256];
+	// 背景のハンドル
+	int m_hBackGround;
+	// フォントのハンドル
+	int m_fontHandle;
 	// フェード処理
 	int m_fadeBright;
 	// フェード速度
