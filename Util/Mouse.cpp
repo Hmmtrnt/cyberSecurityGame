607c78bf63667c5bac1b154e6bdf05e195e1b6f3
Hmmtrnt <rento5310@gmail.com>
2022-11-15 15:00:38 +0900

    情報取得のため変数変更中

--- Util/Mouse.cpp
+++ Util/Mouse.cpp
@@ -3,32 +3,38 @@
 
 Mouse::Mouse() :
 	m_button(0),
-	m_clickX(0),
-	m_clickY(0),
+	/*m_clickX(0),
+	m_clickY(0),*/
 	m_logType(0),
 	m_drawFlag(false),
-	m_drawX(0),
-	m_drawY(0),
-	m_drawColor(0)
+	/*m_drawX(0),
+	m_drawY(0),*/
+	m_drawColor(0),
+	m_click(0),
+	m_draw(0)
 {
-
+	m_click = new Vec2;
+	m_draw = new Vec2;
 }
 
 Mouse::~Mouse()
 {
-
+	delete m_click;
+	delete m_draw;
 }
 
 void Mouse::init()
 {
 	m_button = 0;
-	m_clickX = 0;
-	m_clickY = 0;
+	/*m_clickX = 0;
+	m_clickY = 0;*/
 	m_logType = 0;
 	m_drawFlag = false;
-	m_drawX = 0;
-	m_drawY = 0;
+	/*m_drawX = 0;
+	m_drawY = 0;*/
 	m_drawColor = 0;
+	m_click = 0;
+	m_draw = 0;
 }
 
 void Mouse::end()
@@ -39,14 +45,14 @@ void Mouse::end()
 void Mouse::update()
 {
 	// マウスのボタンが押されたり離されたりしたかどうかの情報を取得する
-	if (GetMouseInputLog2(&m_button, &m_clickX, &m_clickY, &m_logType, TRUE) == 0)
+	if (GetMouseInputLog2(&m_button, &m_click.x, &m_click.y, &m_logType, TRUE) == 0)
 	{
 		// 左ボタンが押されたり離されたりしていたら描画するかどうかのフラグを立てて、座標も保存する
 		if ((m_button & MOUSE_INPUT_LEFT) != 0)
 		{
 			m_drawFlag = TRUE;
-			m_drawX = m_clickX;
-			m_drawY = m_clickY;
+			m_draw.x = m_click.x;
+			m_draw.y = m_click,y;
 
 			// 四角形の色を押された場合は白に、離された場合は紫にする
 			m_drawColor = m_logType == MOUSE_INPUT_LOG_DOWN ? GetColor(255, 255, 255) : GetColor(255, 0, 255);
--- Util/Mouse.h
+++ Util/Mouse.h
@@ -22,15 +22,20 @@ protected:
 	// 押されたり離され足りするボタンの値
 	int m_button;
 	// クリックした座標
-	int m_clickX;	// X
-	int m_clickY;	// Y
+//	int m_clickX;	// X
+//	int m_clickY;	// Y
 	// ボタンが押されたのかまたは離されたのかの情報取得
 	int m_logType;
 	// 描画するかどうか
 	int m_drawFlag;
 	// 描画座標
-	int	m_drawX;		// X
-	int	m_drawY;		// Y
+//	int	m_drawX;	// X
+//	int	m_drawY;	// Y
 	// 描画色
 	int m_drawColor;
+
+	// クリックした座標
+	Vec2* m_click;
+	// 描画座標
+	Vec2* m_draw;
 };
