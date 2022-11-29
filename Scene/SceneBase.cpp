6ca208692c350671a26596e24bed065f934ed7e0
Hmmtrnt <rento5310@gmail.com>
2022-11-29 17:28:13 +0900

    コード修正、整理

--- Scene/SceneBase.cpp
+++ Scene/SceneBase.cpp
@@ -7,11 +7,16 @@ void SceneBase::inputKey(int key[256])
 {
     static char buf[256];
     GetHitKeyStateAll(buf);
-    for (int i = 0; i < 256; i++) {
-        if (buf[i]) {
+    for (int i = 0; i < 256; i++) 
+    {
+        if (buf[i]) 
+        {
             if (key[i] == 0) key[i] = 1;
             else if (key[i] == 1) key[i] = 2;
         }
-        else key[i] = 0;
+        else
+        {
+            key[i] = 0;
+        }
     }
 }
